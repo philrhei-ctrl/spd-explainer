36268f2244ed23913dd6e41964aa239b610bbf8c
Phil Hein <philrhei@gmail.com>
2025-08-24 11:38:33 -0600

    Move app/ files into src/app/ so Next.js can find layouts

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>SPD Explainer</h1>
-      <p>Deployed on Vercel 🎉</p>
-      <p><a href="/upload">Go to Upload</a></p>
-    </main>
-  );
-}
